2b06bc72387881480cdd036e080a01d68da2a5d5
Ken Rutan and Spencer Alan <ken@>
2017-05-30 16:04:00 -0700

    add all files necessary for reviews

---
+++ app/routes/index.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review'),
+    });
+  },
+
+  actions: {
+    saveRental3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
+    saveReview3(params) {
+      var newReview = this.store.createRecord('review', params);
+      newReview.save();
+      this.transitionTo('index');
+    },
+
+  }
+});
